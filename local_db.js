cc4470e91d56e2d729f43a8ce49fadc5cc15bda5
chavan-gopal <gopal.chavan@celigo.com>
2024-02-20 15:48:08 +0530

    auto delete with default value w.r.t createAt filed

--- local_db.js
+++ local_db.js
@@ -8,7 +8,20 @@ const Heartbeat = new Schema({
   key: {type: String, required: true, unique: true},
   timestamp: {type: Date, required: true},
   uptime: {type: Number},
-  deletedAt: { type: Date, default: null, index: { expireAfterSeconds: 0 } }
+  deletedAt: {
+    type: Date, 
+    default: function () {
+      // Set the default value to 30 days from the document's creation date
+      const afterOneMinFromNow = new Date(this.createdAt);
+      afterOneMinFromNow.setMinutes(afterOneMinFromNow.getMinutes() + 2)
+      // const thirtyDaysFromNow = new Date(this.createdAt);
+      // thirtyDaysFromNow.setDate(thirtyDaysFromNow.getDate() + 30);
+      return afterOneMinFromNow;
+    },
+ 
+    index: { expireAfterSeconds: 0 }
+  }
+
 })
 
 function getLocalDBUrl(){
@@ -54,14 +67,31 @@ async function insertRecs_local() {
   // expirationTime.setDate(expirationTime.getDate() + dataRetentionDays);
 
   const recs = [{
-    key: 'some_key_8',
+    key: 'some_key_9',
     timestamp: new Date(),
     uptime: 10,
     deletedAt: expirationTime,
   }]
   const MyModel = model.getInstance()
+  // uncomment to check auto-delete functionality with default value
+  // create_with_delay()
   const res = await MyModel.insertMany(recs)
-  console.log(res)
+  console.log("after creation", new Date())
+}
+
+async function create_with_delay() {
+  console.log('settimeout', new Date())
+  setTimeout(async function() {
+    console.log('now inserting record-----------------------after 3 min')
+    const recs = [{
+      key: 'some_key_10',
+      timestamp: new Date(),
+      uptime: 10
+    }];
+    const MyModel = model.getInstance()
+    const res = await MyModel.insertMany(recs)
+    console.log('settimeout after creation', new Date())
+  }, 2*60*1000)
 }
   
 module.exports = {
